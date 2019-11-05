44370f62326972e751fb66df012afb5095e31ea4
robertobirijnr <robertobirijnr@hotmail.com>
2019-11-05 08:59:48 +0000

    navbar with inner pages

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import './App.css';
-import Movies from './components/movies';
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Rentals from "./components/rentals";
+import Customers from "./components/customers";
+import NotFound from "./components/notFound";
+import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
+import NavBar from "./components/navBar";
+import "./App.css";
+import LoginForm from "./components/login";
 
-function App() {
-  return (
-    <div className="App">
-      <Movies/>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<React.Fragment>
+				<NavBar />
+				<main className="container">
+					<Switch>
+						<Route path="/login" component={LoginForm}></Route>
+						<Route path="/movies/:id" component={MovieForm}></Route>
+						<Route path="/movies" component={Movies}></Route>
+						<Route path="/customers" component={Customers}></Route>
+						<Route path="/rentals" component={Rentals}></Route>
+						<Route path="/not-found" component={NotFound}></Route>
+						<Redirect from="/" exact to="/movies" />
+						<Redirect to="/not-found" />
+					</Switch>
+				</main>
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
---
+++ music-app/src/components/login.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Input from "./common/input";
+
+class LoginForm extends Component {
+	state = {
+		account: {
+			username: "",
+			password: ""
+		}
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+	};
+
+	handleChange = ({ currentTarget: input }) => {
+		const account = { ...this.state.account };
+		account[input.name] = input.value;
+		this.setState({ account });
+	};
+
+	render() {
+		const { account } = this.state;
+		return (
+			<div>
+				<h1>Login</h1>
+				<form onSubmit={this.handleSubmit}>
+					<Input
+						name="username"
+						value={account.username}
+						label="Username"
+						onChange={this.handleChange}
+					/>
+					<Input
+						name="password"
+						value={account.password}
+						label="Password"
+						onChange={this.handleChange}
+					/>
+					<button className="btn btn-primary">Login</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default LoginForm;
